8135f6c1def218e6dcd27ac2a288b3cddb6e1669
Joe D <j.dimartino@live.com>
2020-06-29 14:30:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,15 +10,13 @@ def load_library(library_path)
       :japanese => value[1]
     }
   end
-  p dictionary
+  dictionary
 end
 
-load_library('lib/emoticons.yml')
+def get_japanese_emoticon ('lib/emoticons.yml', emoticon)
+  # code goes here
+end
 
-# def get_japanese_emoticon ('lib/emoticons.yml', emoticon)
-#   # code goes here
-# end
-#
-# def get_english_meaning ('lib/emoticons.yml', emoticon)
-#   # code goes here
-# end
+def get_english_meaning ('lib/emoticons.yml', emoticon)
+  # code goes here
+end
