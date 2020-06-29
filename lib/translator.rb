0521456328988133a960198c30d73a1642c4c4d9
Joe D <j.dimartino@live.com>
2020-06-29 14:23:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,18 +1,20 @@
 # require modules here
 require "yaml"
 
-dict = YAML.load_file('lib/emoticons.yml')
+def load_library(libray_path)
+  # code goes here
+  dictionary = YAML.load_file(library_path)
+  dictionary.each do |key, value|
+    key[:english] = value[0]
+    key[:japanese] = value[1]
+  end
+  dictionary
+end
 
-puts dict
-
-def load_library
+def get_japanese_emoticon ('lib/emoticons.yml', emoticon)
   # code goes here
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
+def get_english_meaning ('lib/emoticons.yml', emoticon)
   # code goes here
 end
